8478190c1cfee9e5d2d36c940cb64e22a37f76b6
verxirtam <d-kosuga@jcom.home.ne.jp>
2019-02-11 06:04:30 +0900

    compile.shにpreonic/rev3を追加

--- compile.sh
+++ compile.sh
@@ -4,13 +4,15 @@ cd `dirname $0`
 
 make satan:verxirtam
 
-make preonic:verxirtam
+make preonic/rev2:verxirtam
 
 make fortitude60/rev1:verxirtam
 
 make ergodash/rev2:verxirtam
 
 make namecard2x4:verxirtam
+
+make preonic/rev3:verxirtam
 
 cp *.hex /mnt/LS-VL6D2/daisuke/documents/keyboad/qmk_firmware/
 
